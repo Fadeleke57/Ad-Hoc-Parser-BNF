cffd8cded6529964c42aa6f102faaabfc0c25879
Farouk Adeleke <110058327+Fadeleke57@users.noreply.github.com>
2024-03-29 00:55:27 -0400

    Update assign07.ml

--- assign07.ml
+++ assign07.ml
@@ -44,59 +44,7 @@ let span (p : 'a -> bool) (l : 'a list) : 'a list * 'a list =
 
 (* ============================================================ *)
 
-(* Homework 7 : Parsing BNF Specifications
-
-   In this assignment, you will be implementing an ad hoc parser for
-   BNF specifications (without alternatives).
-
-   A terminal symbol is represented as a nonempty contiguous sequence
-   of lowercase letters.
-
-   Examples: "abc" "aaaa" "x" "example"
-
-   A nonterminal symbols is represented by a nonempty contiguous
-   sequence of lowercase letters surrounded by '<' and '>'.  In
-   particular, there can be no whitespace between the brackets.
-
-   Examples: "<abc>" "<aaaa>" "<x>" "<example>"
-
-   A rule is represented by a nonterminal symbol, followed by the
-   symbol '::=', followed by a sequence of symbols, and ending in '.'
-   a period symbol.  Rules are whitespace agnostic except that
-   consecutive terminal symbols are must be separated by at least one
-   space.
-
-   Examples:
-   "<abc> ::= <aaaa><x><example>."
-   "<abc> ::= <aaaa>   <x>   <example>    .      "
-   "<a>::=a  asdf  asdf  <x><y>."
-
-   A grammar is represented by a sequences of rules.
-
-   Example:
-   "<abc> ::= <aaaa><x><example>.
-    <abc> ::= <aaaa>   <x>   <example>.
-    <a>::=a  asdf  asdf  <x><y>."
-*)
-
-(* ============================================================ *)
-
-(* Problem 1: Tokenizing
-
-   Implement the function `next_token` which, given
-
-     cs : char list
-
-   returns the token for the first lexeme in `cs`, ignoring leading
-   whitespace, along with the remainder of the characters after the
-   end of the lexeme.
-
-   The EOFT token should be returned in the case `next_token` is
-   called on the empty list.
-
-   Hint: The function `span` above may be useful.
-
-*)
+(* Tokenizing*)
 
 type token
   =
@@ -178,41 +126,9 @@ let _ = assert(tokenize "..::=" = Some [PdT;PdT;EqT])
 let _ = assert(tokenize "<a> ::= aab a<b>a." = Some [NtmT "a"; EqT; TmT "aab"; TmT "a"; NtmT "b"; TmT "a"; PdT])
 let _ = assert(tokenize "<a> ::= aab a<no-good>a." = None)
 
-
-(* END OF PROBLEM 1 *)
-
 (* ============================================================ *)
 
-(* Problem 2: ADTs
-
-   A symbol is abstractly represented by an ADT with variants `NT` and
-   `T`, each carrying a string representing the identifier for the
-   symbol.
-
-   A sentential form is represented as a list of symbols.
-
-   A rule is represented as a string (the identifier of a nonterminal
-   symbol) and a sentential form where, for example
-
-     ("id", [T "a"; NT "b"; T "c"])
-
-   represents the rule
-
-     <id> ::= a <b> c .
-
-   A grammar is represented as a list of rules.
-
-   ============================================================
-
-   Implement the function `expand_leftmost` which, given
-
-     (nt, sf) : rule
-     s : sent_form
-
-   expands the leftmost occurrence of the nonterminal symbol `<nt>` in
-   `s` (if it exists) to the sentential form `sf`.
-
-*)
+(* ADTs *)
 
 type symbol
   = NT of string
@@ -247,31 +163,9 @@ let _ = assert (expand_leftmost r (expand_leftmost r [NT "b"]) = [T "b"; NT "a";
 (* <a>b<a> --> a<a>b<a> *)
 let _ = assert (expand_leftmost r [NT "b"; T "d"; NT "a"; NT "b"] = [T "b"; NT "a"; T "b"; T "d"; NT "a"; NT "b"])
 
-
-(* END OF PROBLEM 2 *)
-
 (* ============================================================ *)
 
-(* Problem 3: Parsing BNF Specifications
-
-   Implement the following functions for parsing a BNF specification,
-   as described above.  This is, in essence, an ad hoc
-   recursive-descent parser.
-
-   `parse_sent_form` gets the LONGEST NONEMPTY sentential form from
-   the beginning of a list of tokens, also returning the remaining
-   tokens.  Note that we return `None` in the case that the list of
-   tokens does not start with a symbol.
-
-   `parse_rule` gets a single rule from the beginning of a list of
-   tokens, also returning the remaining tokens.
-
-   `parse_grammar` gets the LONGEST sequence of rules from the
-   beginning of a list of tokens, also returning the remaining tokens.
-   Note that there is no need to return an option because the list of
-   rules can be empty.  The order or rules should be maintained.
-
-*)
+(* Parsing BNF Specifications*)
 
 let rec parse_sentform (ts : token list) : (sentform * token list) option =
   let rec extract_valid_tokens tokens acc = (*returns the first k valid tokens and the rest of the list*)
@@ -385,6 +279,3 @@ let simple_test_missing_period = "
 
 let _ = assert (parse_and_check simple_test = Some simple_test_out)
 let _ = assert (parse_and_check simple_test_missing_period = None)
-
-
-(* END OF PROBLEM 3 *)
